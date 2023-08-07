6f7dd689fca69e997d0843a20f676723c7910785
Federico <fechudefe@gmail.com>
2023-08-06 21:52:29 -0300

    actualizando los precios de los productos

--- productos.js
+++ productos.js
@@ -10,14 +10,14 @@ class Producto {
   }
 }
 //Creando los objetos de productos
-let producto1 = new Producto(1, "Gomitas", "./img/mogul-acidas.jpg", 10, 120);
-let producto2 = new Producto(2, "Alfajor Jorgito", "./img/alfajor-jorgito.jpg", 2, 300);
-let producto3 = new Producto(3, "Cerveza", "./img/imperial-golde.jpg", 5, 3000);
-let producto4 = new Producto(4, "Sprite", "./img/sprite.jpg", 10, 1050);
+let producto1 = new Producto(1, "Gomitas", "./img/mogul-acidas.jpg", 10, 250);
+let producto2 = new Producto(2, "Alfajor Jorgito", "./img/alfajor-jorgito.jpg", 2, 250);
+let producto3 = new Producto(3, "Cerveza", "./img/imperial-golde.jpg", 5, 580);
+let producto4 = new Producto(4, "Sprite", "./img/sprite.jpg", 10, 1000);
 let producto5 = new Producto(5, "Alfajor Block", "./img/alfajor-block.jpg", 25, 320);
-let producto6 = new Producto(6, "Pack Imperial Ipa", "./img/pack-imperial-ipa.jpg", 35, 100);
-let producto7 = new Producto(7, "Mogul Acidas Tira", "./img/mogul-acidas-tira.jpg", 25, 270);
-let producto8 = new Producto(8, "Kinder Sorpresa", "./img/kinder-sorpresa.png", 50, 200);
+let producto6 = new Producto(6, "Pack Imperial Ipa", "./img/pack-imperial-ipa.jpg", 35, 3000);
+let producto7 = new Producto(7, "Mogul Acidas Tira", "./img/mogul-acidas-tira.jpg", 25, 170);
+let producto8 = new Producto(8, "Kinder Sorpresa", "./img/kinder-sorpresa.png", 50, 450);
 
 //Creando el array de la lista de productos y pusheandolos al interior
 const productos = [];
